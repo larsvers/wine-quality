2dbcfb2ea1f114660f624c31353925fea9a38be0
larsvers <larsverspohl@gmail.com>
2020-06-26 10:52:49 +0100

    Add fn to calculate path transform
      My svg images are all different sizes.
      To fit them into the container, I calculate
      their translate and scales.
    
      Viewbox can do this too, but I assume that'll be
      more complex later when we want to set chart elements
      next to the images.

--- src/app/init.js
+++ src/app/init.js
@@ -1,139 +1,152 @@
-import { select } from 'd3-selection/src/index';
+import { select, selection } from 'd3-selection/src/index';
 import { gsap } from 'gsap/all';
 import { MorphSVGPlugin } from 'gsap/src/MorphSVGPlugin';
 import { splitPathString, toPathString } from 'flubber/index';
-import shot from '../../static/wine-scape-s';
+import scape from '../../static/wine-scape-s';
 import glass from '../../static/wine-glass';
 import bottle from '../../static/wine-bottle';
 
 gsap.registerPlugin(MorphSVGPlugin);
 
-function updateInitialImage() {
-  // Elements
-  const svg = select('#svg-main-container');
-  const initialImage = svg.select('#initial-image');
+// Utils
+// -----
 
-  // Base measures.
-  const initialImageBox = initialImage.node().getBBox();
-  const visualWidth = svg.node().offsetWidth;
-  const visualHeight = svg.node().offsetHeight;
-
-  // Target measures.
-  const scale = visualWidth / initialImageBox.width;
-  const originHeight = visualHeight / 2 - (initialImageBox.height * scale) / 2;
-
-  // Update.
-  // initialImage.attr(
-  svg
-    .select('g')
-    .attr(
-      'transform',
-      `translate(0, ${originHeight}) scale(${scale}, ${scale})`
-    );
+function isSelection(el) {
+  if (typeof el === 'string') return false;
+  if (el instanceof selection) return true;
+  return false;
 }
 
-function resize() {
-  updateInitialImage();
+function getBox(el) {
+  const sel = isSelection(el) ? el : select(el);
+  return sel.node().getBBox();
 }
 
-function addTestElements(parent) {
-  parent
-    .append('circle')
-    .attr('class', 'circle')
-    .attr('cx', 100)
-    .attr('cy', 100)
-    .attr('r', 20)
-    .style('fill', 'none')
-    .style('stroke', 'black');
-
-  parent
-    .append('rect')
-    .attr('class', 'rect')
-    .attr('x', 100)
-    .attr('y', 100)
-    .attr('width', 50)
-    .attr('height', 50)
-    .style('fill', 'none')
-    .style('stroke', 'black');
-
-  MorphSVGPlugin.convertToPath('.circle, .rect');
+function setTransform(el, t) {
+  const sel = isSelection(el) ? el : select(el);
+  sel.attr(
+    'transform',
+    `translate(${t.x}, ${t.y}), scale(${t.scale}, ${t.scale})`
+  );
 }
 
-let index = 350;
-let imgPathSplit = [];
+/**
+ * This fn calculates the translate and scale values for the object based on the parent visual's
+ * dimensions. `fit` decides how big in relation to width or height it should be (width: 1 would
+ * be full width). Note, it can only fit towards one measure as skewing is not worked in.
+ * `pos` decides which x, y place it should have. Leave this empty if you want to centre the image.
+ * @param { SVGRect|Object } object The object's bounding box - at least with width and height
+ * @param { Object } fit  { width: 0-1, height: 0-1 } The percentage of the width or height the
+ *                        object should fit with. One must be 0!
+ * @param { Object } pos  { x: 0-1, y: 0-1 } The percentage of the width or height
+ *                        x or y should be at
+ * @returns { Object }    { x, y, scale } The translate and scale values to be used for positioning.
+ */
+function getTransform(object, fit, pos) {
+  if (fit.width !== 0 && fit.height !== 0)
+    throw Error('One fit value must be 0');
 
-function findGlassPath() {
-  select('#svg-main')
-    .select('.hello')
-    .remove();
+  // Get the parent visual.
+  const frame = select('#svg-main-container').node();
+  const visual = {
+    width: frame.offsetWidth,
+    height: frame.offsetHeight,
+  };
 
-  select('#svg-main')
-    .append('g')
-    .attr('class', 'hello')
-    .attr('transform', `translate(0, 500) scale(0.5, 0.5)`)
-    .append('path')
-    .style('fill', 'none')
-    .style('stroke-width', 1)
-    .style('stroke', 'black')
-    .attr('d', imgPathSplit[index]);
+  // Establish the object scale. fit.width and fit.height decide the scale
+  // in relation to either the height or the width. You can't have both,
+  // becasue we won't skew. These two term ↓ don't become a sum, but one
+  // will be 0 when the other one is not.
+  const scale =
+    (visual.width / object.width) * fit.width +
+    (visual.height / object.height) * fit.height;
 
-  console.log(index);
-  console.log(imgPathSplit[index]);
-  index++;
+  // Establish the object's position. There's pos.width and pos.height,
+  // both expressed as % of the width or the height. The percentages
+  // will be transformed to x and y. If pos is undefined or set to false,
+  // the object will be centered.
+  let position = { x: null, y: null };
+  if (pos) {
+    position.x = visual.width * pos.x;
+    position.y = visual.height * pos.y;
+  } else {
+    position.x = visual.width / 2 - (object.width * scale) / 2;
+    position.y = visual.height / 2 - (object.height * scale) / 2;
+  }
 
-  // gsap.to('#initial-image', { duration: 2, morphSVG: '.circle' });
+  // Nice the values all up.
+  return {
+    x: Math.round(position.x),
+    y: Math.round(position.y),
+    scale: Math.round(scale * 100) / 100,
+  };
 }
 
-function getBoxDims() {
-  const box = select('#svg-main g')
-    .node()
-    .getBBox();
-  console.log(box);
+function updateImage() {
+  const imageGroup = select('#image-group');
+  const box = getBox(imageGroup.select('path'));
+  const fit = { width: 1, height: 0 };
+  const transform = getTransform(box, fit);
+  setTransform(imageGroup, transform);
 }
 
 function glassToBottle() {
-  getBoxDims();
-  gsap
-    .timeline({ onComplete: getBoxDims })
-    .to('#wine-glass', { duration: 3, morphSVG: '#bottle' });
+  gsap.to('#wine-glass', { duration: 3, morphSVG: '#bottle' });
+}
+
+function resize() {
+  updateImage();
 }
 
 function init() {
   const svg = select('#svg-main');
-  const group = svg.append('g');
+  const group = svg.append('g').attr('id', 'image-group');
 
   // Full wine scape svg.
   group
     .append('path')
-    .attr('id', 'initial-image')
-    .attr('d', shot)
+    .attr('id', 'wine-scape-path')
+    .attr('d', scape)
     .style('fill', 'none')
     .style('stroke-width', 1)
     .style('stroke', 'grey');
 
-  // Just glass section from wine scape svg.
-  group
-    .append('path')
-    .attr('id', 'wine-glass')
-    .attr('d', glass)
-    .style('fill', 'none')
-    .style('stroke-width', 1)
-    .style('stroke', 'red');
+  updateImage();
 
-  // The bottle as morph target (hidden).
-  svg
-    .append('path')
-    .attr('d', bottle)
-    .attr('id', 'bottle')
-    .style('fill', 'none')
-    .style('stroke-width', 1)
-    .style('visibility', 'none');
+  // To animate scape to bottle:
 
-  // Scale and translate the initial image.
-  updateInitialImage();
+  // 1. Overlay scape path with glass path
+  // 2. Add bottle path - hide it.
+  // 3. On button click:
+  // 3.1 Calculate end transform values for path
+  // 3.2 Animate
+  // 3.2.1 morph bottle to glass
+  // 3.2.2 animate group transform values
 
-  // imgPathSplit = splitPathString(select('#initial-image').attr('d'));
-  // addTestElements(svg);
+  // General architecture:
+
+  // init() just add all paths you want to show or might want to transition to
+  // resize() whatever we have to run if we want to resize things
+  // listner() setting up the listener (run from init)
+  // handler() all the handling / animations (could be in ther own, see when you get there)
+
+  // // Just glass section from wine scape svg.
+  // group
+  //   .append('path')
+  //   .attr('id', 'wine-glass')
+  //   .attr('d', glass)
+  //   .style('fill', 'none')
+  //   .style('stroke-width', 1)
+  //   .style('stroke', 'red');
+
+  // // The bottle as morph target (hidden).
+  // svg
+  //   .append('path')
+  //   .attr('d', bottle)
+  //   .attr('id', 'bottle')
+  //   .style('fill', 'none')
+  //   .style('stroke-width', 1)
+  //   .style('visibility', 'none');
 
   window.addEventListener('resize', resize);
   select('#scape-to-bottle').on('click', glassToBottle);
